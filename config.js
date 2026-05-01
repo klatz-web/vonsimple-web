7afe7041285dba6a8eee010327032661460d2090
Admin <admin@example.com>
2026-05-01 17:57:47 +0800

    remove error and fix backend url

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-window.BACKEND_URL = 'https://vonsimple-web-4.onrender.com'; // Backend host only; script appends /api automatically
+window.BACKEND_URL = 'https://vonsimple-web-7.onrender.com'; // Backend host only; script appends /api automatically
